9f67cf2f07fe34ad12fed938f832b7e5e0bba510
AliMartin <info@alimartin.com>
2026-06-12 13:47:58 +0100

    Drupal 11 base

--- sites/default/settings.local.php
+++ sites/default/settings.local.php
@@ -3,17 +3,11 @@
 $settings['update_free_access'] = FALSE;
 $settings['file_private_path'] = DRUPAL_ROOT . '/private-files';
 $settings['file_temp_path'] = DRUPAL_ROOT . '/tmp';
-
-
+$settings['config_sync_directory'] = DRUPAL_ROOT . '/sync';
 
 $settings['trusted_host_patterns'] = [
   '^musetributeband\.com$',
   '^.+\.musetributeband\.com$',
-  '^musetributeband\.hyperluminal\.co\.uk$',
-  '^.+\.musetributeband\.hyperluminal\.co\.uk$',
-  '^musetributeband10\.ddev\.site$',
-  '^.+\.musetributeband10\.ddev\.site$',
+  '^musetributeband11\.ddev\.site$',
+  '^.+\.musetributeband11\.ddev\.site$',
 ];
-
-$settings['config_sync_directory'] = DRUPAL_ROOT . '/sync';
-
